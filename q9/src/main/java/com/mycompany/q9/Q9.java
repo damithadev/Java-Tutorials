ddd9b104373c7b7ccf2b94496b8b6d72b39b4c3b
Damitha Jayatharaka <wimalwimal579@gmail.com>
2023-09-21 19:22:11 +0530

    Add files via upload

---
+++ q9/src/main/java/com/mycompany/q9/Q9.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.q9;
+
+/**
+ *
+ * @author HP
+ */
+public class Q9 {
+
+    public static void main(String[] args) {
+        class Bicycle{
+           private void Braking(){
+               System.out.println("braking");
+           }
+        }
+
+         Bicycle Obj = new Bicycle();
+            
+         Obj.Braking();
+         System.out.println();
+    }
+}
+
