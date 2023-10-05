5330f58e416a13d20f33ffc28d586eb87e956f5d
Damitha Jayatharaka <wimalwimal579@gmail.com>
2023-10-05 14:49:47 +0530

    Add files via upload

---
+++ q10/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+import java.util.Scanner;
+/**
+ *
+ * @author HP
+ */
+public class Mavenproject1 {
+
+    
+    
+    public static void main(String[] args) {
+        
+        student1 obj1 = new student1();
+        obj1.func();
+    }
+    
+    
+}
